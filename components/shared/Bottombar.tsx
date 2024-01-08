c1a2a461a1d810f226ce02201fb37dfc23323617
Illy Hoang <illyhoang@Illys-MacBook.local>
2024-01-07 20:56:23 -0500

    adding bottom bar functionality for phones and tablet devices

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -1,6 +1,33 @@
+"use client"    //  telling the server this is a client-side-rendered component to make webhook operable
+
+import { sidebarLinks } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";  //  allows tracking URL of sidebar widget user is on
+
 function Bottombar() {
+//  for site traversal on mobile devices
+//  essentially same functionality as LeftSidebar()
+    const pathname = usePathname();
+    
     return (
-        <h1>Bottombar</h1>
+        <section className="bottombar">
+            <div className="bottombar_container">
+                {sidebarLinks.map((link) => {
+                    const isActive = (pathname.includes(link.route) && link.route.length > 1) || pathname === link.route;
+                    
+                    return (
+                        <Link 
+                            href={link.route} 
+                            key={link.label} 
+                            className={`bottombar_link ${isActive && 'bg-primary-500'}`}
+                        >
+                            <Image src={link.imgURL} alt={link.label} width={24} height={24}/>
+                            <p className="text-subtle-medium text-light-1 max-sm:hidden">{link.label.split(/\s+/)[0]}</p>
+                        </Link>
+                    )})}
+            </div>
+        </section>
     )
 }
 
