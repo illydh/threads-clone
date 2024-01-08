e78f5842c730a2b91d20dc78603708e68642b73d
Illy Hoang <illyhoang@Illys-MacBook.local>
2024-01-07 19:28:52 -0500

    adding homepage functionality (topbar) and svgs to site

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,12 @@
-//app/page.tsx
+//  app/page.tsx
+//  homepage
+
 import { UserButton } from "@clerk/nextjs";
  
 export default function Home() {
   return (
-    <div>
-      <UserButton afterSignOutUrl="/"/>
-    </div>
+    <>
+      <h1 className="head-text text-left">Home</h1>
+    </>
   )
 }
